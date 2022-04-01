dcf5a40744f0e2a36a0eefc192a828b11046d320
Bulat <bulat.gumerov.z@gmail.com>
2022-04-01 14:34:45 +0400

    Added mac m1(arm64) support

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+set -e -o pipefail
+# Ensure the ppp device exists
+[ -c /dev/ppp ] || su-exec root mknod /dev/ppp c 108 0
 /usr/bin/glider -listen :8443 &
 echo "http/socks5 proxy server: $(hostname -i):8443"
 exec "$@"
